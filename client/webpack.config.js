1c3dd8c758695bae5aa9ea94a69a244e8b141f4a
Steve Calla <callasteven@gmail.com>
2022-10-25 12:39:55 -0600

    Update webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,10 +1,14 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
+// Require the GenerateSW class of the WorkBoxPlugin 
+const WorkboxPlugin = require('workbox-webpack-plugin');
+// Require the GenerateSW class of the manifest 
+const { InjectManifest } = require('workbox-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 // TODO: Add CSS loaders and babel to webpack.
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = () => {
   return {
@@ -18,12 +22,91 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      new MiniCssExtractPlugin(),
+      new WorkboxPlugin.GenerateSW({
+        // Exclude images from the precache
+        exclude: [/\.(?:png|jpg|jpeg|svg)$/],
+
+        // Define runtime caching rules.
+        runtimeCaching: [{
+          // Match any request ends with .png, .jpg, .jpeg or .svg or .gif.
+          urlPattern: /\.(?:png|svg|jpg|jpeg|gif)$/,
+          handler: 'NetworkFirst',
       
+          options: {
+            // Use a custom cache name.
+            cacheName: 'images',
+      
+            // Only cache 2 images.
+            expiration: {
+              maxEntries: 2,
+            },
+          },
+        }],
+      }),  
+      // TODO: Create a manifest.json:
+      new WebpackPwaManifest({
+        name: "TODOs Manifest Example",
+          short_name: "Manifest",
+          orientation: "portrait",
+          display: "standalone",
+          start_url: "./",
+          description: "Keep track of important tasks!",
+          background_color: "#7eb4e2",
+          theme_color: "#7eb4e2",
+          icons: [
+            {
+              src: "./src/images/icon_96x96.png",
+              type: "image/png",
+              sizes: "96x96",
+              purpose: "any"
+            },
+            {
+              src: "./src/images/icon_128x128.png",
+              type: "image/png",
+              sizes: "128x128",
+              purpose: "any"
+            },
+            {
+              src: "./src/images/icon_192x192.png",
+              type: "image/png",
+              sizes: "192x192",
+              purpose: "maskable"
+            },
+            {
+              src: "./src/images/icon_512x512.png",
+              type: "image/png",
+              sizes: "512x512",
+              purpose: "any"
+            }
+          ],
+        }),      
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
