e0e9e97b6a9c9d3fc0fc92e818d4c5ad212478fd
Steve Calla <callasteven@gmail.com>
2022-10-26 11:38:53 -0600

    Adjust images

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -104,10 +104,10 @@ module.exports = () => {
           test: /\.css$/i,
           use: [MiniCssExtractPlugin.loader, 'css-loader'],
         },
-        // {
-        //   test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        //   type: 'asset/resource',
-        // },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
         {
           test: /\.m?js$/,
           exclude: /(node_modules|bower_components)/,
